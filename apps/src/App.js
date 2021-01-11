34e4dd125257090b23d878ff44cd36af5db204c6
Maki Noguchi <makinoguchi@Maki-Noguchi.local>
2021-01-11 17:48:49 +0900

    [add]TODOリスト（Class Component）

--- apps/src/App.js
+++ apps/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDoListItem from "./ToDoListItem.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+
+  // ToDoListをstateに定義、初期値は []
+  state = {
+    todoList: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form
+          className="App-form"
+          onSubmit= { (e) => {
+            // formのデフォルトのイベントをキャンセル
+            e.preventDefault();
+
+            // idがtitleのElementを取得
+            const titleElement = e.target.elements["title"]
+            // idがdescriptionのElementを取得
+            const descriptionElement = e.target.elements["description"];
+
+            // todoList stateに追加
+            this.setState(
+              {
+                todoList: this.state.todoList.concat({
+                  title: titleElement.value,
+                  description: descriptionElement.value
+                })
+              },
+              // stateの変更後に入力した値を空にする
+              () => {
+                titleElement.value = "";
+                descriptionElement.value = "";
+              }
+            )
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <input
+              id="title"
+              placeholder="title"
+            />
+            <textarea
+              id="description"
+              placeholder="description"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+            >
+              登録
+            </button>
+          </div>
+        </form>
+        <div>
+        {/* todoList配列の要素数分ToDoListItemコンポーネントを展開 */}
+          {this.state.todoList.map(todo => (
+            <ToDoListItem
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+              // クリックされたItemをtodoList stateから削除
+              onClick={() => {
+                this.setState({
+                  todoList: this.state.todoList.filter(x => x !== todo)
+                })
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ apps/src/ToDoListItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './ToDoListItem.css';
+
+
+class ToDoListItem extends Component {
+  render(){
+    const {
+      title,
+      description,
+      ...props
+    } = this.props;
+
+    return (
+      <div className="ToDoListItem" {...props}>
+        <div className="ToDoListItem-title">{title}</div>
+        <div className="ToDoListItem-description">{description}</div>
+      </div>
+    );
+  }
+}
+
+export default ToDoListItem;
